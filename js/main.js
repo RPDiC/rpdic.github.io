fff0c9a26a6b236572ca788e584e2d4231506b0e
RPDiC <ryandicintio@hotmail.com>
2016-09-11 22:17:45 -0400

    added jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 function changeJumbo(id1, content1, id2, content2, id3, content3) {
   var contentChange1 = document.getElementById(id1); 
   var contentChange2 = document.getElementById(id2); 
@@ -6,4 +7,20 @@ function changeJumbo(id1, content1, id2, content2, id3, content3) {
   contentChange2.innerHTML = content2;
   contentChange3.innerHTML = content3;
 
-}
+}*/
+
+$(function() {
+  var baseURL = "https://rpdic.github.io/";
+  //load content for about tab
+  $('#about').load(baseURL+'home', function() {
+    $('#myNav').tab(); //initialize tabs 
+  });
+  $('#myNav').bind('show', function(func) {
+    var pattern=/#.+gi 
+    var contentID = func.target.toString().match(pattern)[0];
+    
+    $(contentID).load(baseURL+contentID.replace('#',''), function(){
+      $('#myNav').tab();
+    });
+  });
+});
