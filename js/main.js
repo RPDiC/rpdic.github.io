29d367ecbe532e16b38fc523e343ad98f6a27b17
RPDiC <ryandicintio@hotmail.com>
2016-09-15 18:30:40 -0400

    bleh2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
-/*
-function changeJumbo(id1, content1, id2, content2, id3, content3) {
-  var contentChange1 = document.getElementById(id1); 
-  var contentChange2 = document.getElementById(id2); 
-  var contentChange3 = document.getElementById(id3); 
-  contentChange1.innerHTML = content1;
-  contentChange2.innerHTML = content2;
-  contentChange3.innerHTML = content3;
-
-}*/
-
 var main = function() {
   $('#myNav').on('click','.about',(function() {
-    $('.jumbotron').children().hide();
+    
+    $('.content').hide();
     $('.about').show();
   });
   $('#myNav').on('click','.music',(function() {
-    $('.jumbotron').children().hide();
+    $('.content').hide();
     $('.music').show();
   });
   $('#myNav').on('click','.portfolio',(function() {
